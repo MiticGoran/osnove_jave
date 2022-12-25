684000f3f116df235f61e7aaa3d3ace11a6821f0
Goran Mitic <miticgoran91@gmail.com>
2022-12-25 15:13:23 +0100

    Zadatak za vezbanje (Download bar)

--- src/main/java/domaci_21_12_2022/Zadatak7.java
+++ src/main/java/domaci_21_12_2022/Zadatak7.java
@@ -3,6 +3,13 @@ package domaci_21_12_2022;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//        Za vezbanje jedan
+//        Uvod: Skidanje fajla je po blokovima, u praksi ti blokovi su fiksne velicine ali
+//        nas program radi za proizvoljnu velicinu bloka.
+//        Napisati metodu koja simulira download fajla. Fajl je tekstualnog tipa.
+//        Na pocetku programa korisnik unosi velicinu fajla (broj karaktera koji se skida) a zatim se
+//        sa tastature unosi niz spojenih karaktera (simulacija skinutog bloka) i ispisuje se napredak skidanja.
+//        Program radi dok se ne skine ceo fajl. Na kraju programa stampa se sadrzaj skinutog fajla.
 public class Zadatak7 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -11,7 +18,7 @@ public class Zadatak7 {
         String newBlok = "";
         int blokBroj = 1;
 
-        while (((blokBroj * 100) / velicinaFajla) < velicinaFajla) {
+        while (((blokBroj * 100) / velicinaFajla) < 100) {
             System.out.println("Unesite blok: ");
             String blok = s.next();
             newBlok = newBlok + blok;
